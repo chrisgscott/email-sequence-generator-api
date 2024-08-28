1b3dd54fe3c8faeb5340c6fa844c3ef2b48fda32
chrisgscott <chrisgscott@gmail.com>
2024-08-28 14:23:32 -0600

    Remove sensitive information from alembic.ini

--- alembic/env.py
+++ alembic/env.py
@@ -38,8 +38,8 @@ logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
 
 load_dotenv()  # This loads the variables from .env file, if it exists
 
-# Use the DATABASE_URL from environment variable
-config.set_main_option('sqlalchemy.url', os.getenv('DATABASE_URL'))
+# Override sqlalchemy.url path
+config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
 
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode."""
