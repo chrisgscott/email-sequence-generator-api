0d12ec2d2439abf22d18d928bfd4ac4997193437
chrisgscott <chrisgscott@gmail.com>
2024-08-30 11:34:22 -0600

    Update gunicorn file and delete PROCFILE

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,8 +1,13 @@
 import multiprocessing
 
-workers = 4
+workers = min((multiprocessing.cpu_count() * 2) + 1, 6)  # Cap at 6 workers
 bind = "0.0.0.0:8080"
 worker_class = "uvicorn.workers.UvicornWorker"
 accesslog = "-"
 errorlog = "-"
 loglevel = "debug"
+timeout = 300  # 5 minutes
+keepalive = 120  # How long to wait for requests on a Keep-Alive connection
+
+# Specify the application
+app = "app.main:app"
